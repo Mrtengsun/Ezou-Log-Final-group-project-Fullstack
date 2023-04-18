69a059521a12534ab38bfa026d0a3f3a28ed0cab
Francis Hoffmann <baffoefrancis@icloud.com>
2023-04-18 09:23:34 +0200

    navigation update

--- frontend/src/components/Frontpage/NavigationBar.jsx
+++ frontend/src/components/Frontpage/NavigationBar.jsx
@@ -1,31 +1,42 @@
 import React, { useState } from "react";
 import styles from "./Navigation.module.scss";
-import {BiMenuAltRight} from "react-icons/bi";
-import {AiOutlineCloseSquare} from "react-icons/ai";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineCloseSquare } from "react-icons/ai";
 
 const NavigationBar = () => {
-   const [menuOpen,setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-   const menuToggler = ()=> setMenuOpen((p) => !p);
+  const menuToggler = () => setMenuOpen((p) => !p);
 
   return (
     <nav className={styles.nav}>
       <div className={styles.nav__content}>
-      <div >
-        <span className={styles.logo}>EZOU</span>
-        <br />
-        <span className={styles.logo}>LOG</span>
-      </div>
-      <div>
-        <ul className={`${styles.list} ${menuOpen ? styles[`list--open`] : {}}`} >
-          <li className={styles.list__items}>Home</li>
-          <li className={styles.list__items}>Community</li>
-          <li className={styles.list__items}>About us</li>
-        </ul>
-      </div>
-      <div>
-        <button className={styles.__open} onClick={menuToggler}>{!menuOpen ?<BiMenuAltRight/> : <AiOutlineCloseSquare/>}</button>
-      </div>
+        <div>
+          <span className={styles.logo}>EZOU</span>
+          <br />
+          <span className={styles.logo}>LOG</span>
+        </div>
+        <div>
+          <ul
+            className={`${styles.list} ${menuOpen ? styles[`list--open`] : {}}`}
+          >
+            <li className={styles.list__items}>Home</li>
+            <li className={styles.list__items}>Community</li>
+            <li className={styles.list__items}>About us</li>
+          </ul>
+        </div>
+        <div>
+          {/* <div className={styles.switch}>
+            <input id={styles.checkbox1} type="checkbox" />
+            <label for="checkbox1"></label>
+          </div> */}
+
+          <button className={styles.loginbutton}>Login</button>
+
+          <button className={styles.__open} onClick={menuToggler}>
+            {!menuOpen ? <BiMenuAltRight /> : <AiOutlineCloseSquare />}
+          </button>
+        </div>
       </div>
     </nav>
   );
