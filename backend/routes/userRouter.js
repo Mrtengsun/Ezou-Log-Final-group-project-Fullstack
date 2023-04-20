b54e25c44e1575df4f74ab32861c0e3d9b33dfbe
Diouani <diouanijalouka@gmail.com>
2023-04-20 10:47:01 +0200

    i am adding update router to userRouter

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,7 +3,9 @@ import {
   conformedEmail,
   logIn,
   register,
+  update,
 } from "../controllers/userController.js";
+import checkAuth from "../middlewares/CheckAuth.js";
 
 const router = express.Router();
 
@@ -12,5 +14,6 @@ router.post("/register", register);
 router.get("/register", conformedEmail);
 
 router.post("/login", logIn);
+router.put("/update", checkAuth, update);
 
 export default router;
