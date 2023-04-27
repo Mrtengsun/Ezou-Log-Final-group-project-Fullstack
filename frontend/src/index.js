3f970751f8ceb6aeb095f1d811d527ab5ba9c444
Teng Sun <tengsunstudio@yahoo.com>
2023-04-27 17:33:24 +0200

    Added chatContext and npm socket.io-client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./style/index.scss";
 import App from "./App.jsx";
 import CommunityContextProvider from "./components/contexts/communityContext.js";
-
+import ChatContextProvider from "./components/contexts/chatContext.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <CommunityContextProvider>
-      <App />
+      <ChatContextProvider>
+        <App />
+      </ChatContextProvider>
     </CommunityContextProvider>
   </BrowserRouter>
 );
