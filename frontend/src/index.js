c78fc96d7950fdc61786c5a7b592198b1393e65e
Teng Sun <tengsunstudio@yahoo.com>
2023-05-04 11:49:42 +0200

    edited socket.io

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,9 +11,9 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:5000");
 
-socket.on("connect", () => {
-  console.log(socket.id);
-});
+// socket.on("connect", () => {
+//   console.log(socket.id);
+// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
