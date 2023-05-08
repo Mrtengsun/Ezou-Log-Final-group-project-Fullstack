bbdbe555a0114b5e6eefcc1427c952b0544ac635
Teng Sun <tengsunstudio@yahoo.com>
2023-05-08 11:59:40 +0200

    new chatbot updated

--- frontend/src/components/Frontpage/DarkMode/DarkMode.jsx
+++ frontend/src/components/Frontpage/DarkMode/DarkMode.jsx
@@ -6,28 +6,31 @@ import { useContext } from "react";
 import { ThemeCTX } from "../../../App";
 
 const DarkMode = () => {
-    const {changeTheme,theme,setTheme} = useContext(ThemeCTX)
+  const { changeTheme, theme, setTheme } = useContext(ThemeCTX);
 
-    useEffect(()=>{
-        localStorage.setItem("themeKey",JSON.stringify(theme))
+  useEffect(() => {
+    localStorage.setItem("themeKey", JSON.stringify(theme));
+  }, [theme]);
 
-    },[theme])
-
-    const toggleChange = theme==="light"? "first_mode_label":"second_mode_label"
-    return (
-        <div className='dark_mode'>
-            <input
-                className='dark_mode_input'
-                type='checkbox'
-                id='darkmode-toggle'
-                onChange={changeTheme}
-            />
-            <label className={`dark_mode_label ${toggleChange}`} for='darkmode-toggle'>
-                <Sun />
-                <Moon />
-            </label>
-        </div>
-    );
+  const toggleChange =
+    theme === "light" ? "first_mode_label" : "second_mode_label";
+  return (
+    <div className="dark_mode">
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode-toggle"
+        onChange={changeTheme}
+      />
+      <label
+        className={`dark_mode_label ${toggleChange}`}
+        for="darkmode-toggle"
+      >
+        <Sun />
+        <Moon />
+      </label>
+    </div>
+  );
 };
 
 export default DarkMode;
