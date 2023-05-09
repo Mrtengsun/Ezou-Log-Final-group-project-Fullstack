f17b39d3a40d755f50200edda31f9b5c59e82458
Diouani <diouanijalouka@gmail.com>
2023-05-09 15:33:21 +0200

    modified backend/package.json

--- frontend/src/components/Loginprocess/LoginComponent.jsx
+++ frontend/src/components/Loginprocess/LoginComponent.jsx
@@ -10,52 +10,63 @@ const LoginComponent = () => {
     event.preventDefault();
     console.log(userInput.current.value);
     const loginData = {
-      userName: userInput.current.value,
+      email: userInput.current.value,
       password: passwordInput.current.value,
     };
     const config = {
       method: "Post",
       headers: {
-        "Conten-Type": "application/json",
+        "Content-type": "application/json",
       },
       body: JSON.stringify(loginData),
     };
 
     fetch(`http://localhost:5000/api/user/login`, config)
-      .then(res => res.json())
-      .then(result => {
-        console.log(result)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="cover">
       <div className="loginbox">
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-
           <div className="inputbox">
             <div className="input-container">
               <input type="email" placeholder="username" ref={userInput} />
               <br /> <br />
-              <input type="password" placeholder="password"
-                ref={passwordInput} />
+              <input
+                type="password"
+                placeholder="password"
+                ref={passwordInput}
+              />
             </div>
 
             <div className="forgetpass">
-              <label type="checkbox"><input type="checkbox" className="check" />Remember me</label>
-              <Link className="account" to='/forgotten password'>Forgotten password ?</Link>
+              <label type="checkbox">
+                <input type="checkbox" className="check" />
+                Remember me
+              </label>
+              <Link className="account" to="/forgotten password">
+                Forgotten password ?
+              </Link>
             </div>
-            <button type="submit" className="btn">Sign in</button>
+            <button type="submit" className="btn">
+              Sign in
+            </button>
             <div className="register">
-              <label>Create an Account  </label>
-              <Link className="account" to='/Register'>REGISTER?</Link>
+              <label>Create an Account </label>
+              <Link className="account" to="/Register">
+                REGISTER?
+              </Link>
             </div>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 export default LoginComponent;
