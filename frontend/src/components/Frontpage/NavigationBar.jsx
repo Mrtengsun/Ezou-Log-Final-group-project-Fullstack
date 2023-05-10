af1f21032f9c2727a4183acbd10f466e92699338
Francis Hoffmann <baffoefrancis@icloud.com>
2023-05-10 15:39:39 +0200

    logout pörocess

--- frontend/src/components/Frontpage/NavigationBar.jsx
+++ frontend/src/components/Frontpage/NavigationBar.jsx
@@ -9,20 +9,24 @@ import Darkmode from "./DarkMode/DarkMode";
 const NavigationBar = () => {
 
 
-  const { user,navigate } = useContext(CreateaccountCTX);
+  const { user,navigate,setUser } = useContext(CreateaccountCTX);
 
  const logOut = ()=>{
-  localStorage.clear()
+  localStorage.removeItem("user")
+  localStorage.removeItem("token")
+  setUser(null)
   navigate("/")
  }
 
+
   const [menuOpen, setMenuOpen] = useState(false);
+  const imageURL= user.imgProfile? `http://localhost:5000/api/user/profile-picture/${user._id}`:`/images/Neytiri_Profilbild.webp`;
 
   const logo = () => {
     navigate("/");
   };
   const menuToggler = () => setMenuOpen((p) => !p);
-
+    const community = user? "/community":"/login"
   return (
     <nav className={styles.nav}>
       <div className={styles.nav__content}>
@@ -37,12 +41,12 @@ const NavigationBar = () => {
             className={`${styles.list} ${menuOpen ? styles[`list--open`] : {}}`}
           >
             <li className={styles.list__items}>
-              <NavLink className={styles.link} to="/">
+              <NavLink className={styles.link} to="/homepage">
                 Home
               </NavLink>
             </li>
             <li className={styles.list__items}>
-              <NavLink to="/community">Community</NavLink>
+              <NavLink to={community}>Community</NavLink>
             </li>
             <li className={styles.list__items}>
               <NavLink to="/aboutus">About us</NavLink>
@@ -56,12 +60,12 @@ const NavigationBar = () => {
             {!user?
             /* //login in button will display when user is not log in */
             <button className={styles.loginbutton}>
-              <NavLink to="/loginComponent">Login</NavLink>
+              <NavLink to="/login">Login</NavLink>
             </button>:
             /* logout and avater will display when user is log in  */
             <div className={styles.avatar__logout}>
               <img
-                src="/images/Neytiri_Profilbild.webp"
+                src={imageURL}
                 alt=""
                 className={styles.avatar}
               />
