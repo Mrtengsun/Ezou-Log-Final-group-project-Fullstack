ee33feba8b2828a9a3f8653e65adc06f274c8bdf
Francis Hoffmann <baffoefrancis@icloud.com>
2023-05-23 10:13:56 +0200

    finishing

--- frontend/src/components/Frontpage/HeroComponents.jsx
+++ frontend/src/components/Frontpage/HeroComponents.jsx
@@ -3,22 +3,29 @@ import styles from "./Hero.module.scss";
 import { Link } from "react-router-dom";
 import { CreateaccountCTX } from "../contexts/CreateaccountCTX";
 
-
 const HeroComponents = () => {
-  const {user} = useContext(CreateaccountCTX)
+  const { user } = useContext(CreateaccountCTX);
   return (
     <div className={styles.hero}>
       <div>
-        {!user?
-        <h1 className={styles.header}>SignUp to Secure Your Data</h1>:
-        <h1 className={styles.header}>Speed Uploads of Files</h1>
-      }
+        {!user ? (
+          <h1 className={styles.header}>SignUp to Secure Your Data</h1>
+        ) : (
+          <h1 className={styles.header}>
+            Uploads and Secure your Data with Us
+          </h1>
+        )}
       </div>
       <div>
-        {!user? 
-        <button className={styles.buttm}><Link to="/register">Get Onbord</Link></button>:
-        <button className={styles.buttm}><Link to="/uploads">Upload Files</Link></button>
-      }
+        {!user ? (
+          <button className={styles.buttm}>
+            <Link to="/register">Get Onbord</Link>
+          </button>
+        ) : (
+          <button className={styles.buttm}>
+            <Link to="/uploads">Upload Data</Link>
+          </button>
+        )}
       </div>
     </div>
   );
